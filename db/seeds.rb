994cb98d8806e1d2631cbaa7b63fc3093e6d52e9
Arnaud Floriani <sarkraf.dev@gmail.com>
2024-08-06 17:55:44 +0200

    add Posts CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Article.destroy_all
+
+10.times { Article.create!({ title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph }) }
